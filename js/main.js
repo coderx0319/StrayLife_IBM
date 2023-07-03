bca0075c98b2fd3b263bb56e1087abc004540f2e
Shantanu Vedante <121545998+Shantanu-alt@users.noreply.github.com>
2023-07-03 09:47:22 +0000

    shantanu

--- js/main.js
+++ js/main.js
@@ -67,3 +67,14 @@
     
 })(jQuery);
 
+//login page js content//
+
+const loginsec=document.querySelector('.login-section')
+const loginlink=document.querySelector('.login-link')
+const registerlink=document.querySelector('.register-link')
+registerlink.addEventListener('click',()=>{
+    loginsec.classList.add('active')
+})
+loginlink.addEventListener('click',()=>{
+    loginsec.classList.remove('active')
+})
